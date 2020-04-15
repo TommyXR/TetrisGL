225403232b14c5cba8d4fa32c41c520b4d0e1543
Tommy-Xavier Robillard <tommy.xavier.robillard@gmail.com>
2020-04-14 22:40:56 -0400

    Started migrating from glbinding to glad

--- include/gfx/renderer.hpp
+++ include/gfx/renderer.hpp
@@ -3,6 +3,7 @@
 
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
--- src/gfx/texture.cpp
+++ src/gfx/texture.cpp
@@ -7,9 +7,9 @@
 #include <glbinding/gl/gl.h>
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include "stb/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
+#include "stb/stb_image_write.h"
 
 
 namespace fs = std::filesystem;
