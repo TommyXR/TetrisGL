fc008d5dad918bca47c1880300e5ce6072153cb4
Tommy-Xavier Robillard <tommy.xavier.robillard@gmail.com>
2020-05-10 15:33:35 -0400

    Started working on Windows support

--- src/gfx/texture.cpp
+++ src/gfx/texture.cpp
@@ -7,6 +7,11 @@
 
 #include "glad/glad.h"
 
+#ifdef _WIN64
+#define STBI_WINDOWS_UTF8
+#define _MSC_VER 2000
+#endif
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
@@ -22,9 +27,18 @@ namespace gfx {
 texture2d::texture2d(fs::path file) {
 
     int w, h, channels;
-    auto data{stbi_load(file.c_str(), &w, &h, &channels, 0)};
 
+#ifdef _WIN64
+    auto tmp = file.c_str();
+    char filename[1024];
 
+    auto sz = stbi_convert_wchar_to_utf8(&filename[0], 1024, tmp);
+
+#else
+    auto filename = file.c_str();
+#endif
+
+    auto data = stbi_load(filename, &w, &h, &channels, 0);
 
     width = w;
     height = h;
